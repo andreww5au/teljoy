5c4b785fc248833560a1a6d247bcb9df86558575
Andrew Williams <andrew@physics.uwa.edu.au>
2013-01-14 12:19:29 +0800

    Handle posviolate flag in current position string

--- detevent.py
+++ detevent.py
@@ -85,12 +85,18 @@ class CurrentPosition(correct.CalcPosition):
      allow jumps from this position.
   """
   def __repr__(self):
-    l1 = "Top RA:  %s      LST: %s         ObjID:   %s" % (sexstring(self.RaC/15.0/3600,fixed=True), sexstring(self.Time.LST, fixed=True), self.ObjID)
-    l2 = "Top Dec: %s     UT:  %s" % (sexstring(self.DecC/3600,fixed=True), self.Time.UT.time().isoformat()[:-4])
-    l3 = "Alt:     %s      HA:  %s        ObjRA:   %s" % (sexstring(self.Alt, fixed=True), sexstring(self.RaC/15/3600-self.Time.LST, fixed=True),
-                                                                sexstring(self.Ra/15/3600, fixed=True))
-    l4 = "Airmass: %6.4f        NonSidereal: %s      ObjDec:  %s" % (1/math.cos((90-self.Alt)/180*math.pi), {False:"OFF", True:" ON"}[prefs.NonSidOn],
-                                                                 sexstring(self.Dec/3600, fixed=True))
+    if self.posviolate:
+      l1 = "Top RA:  %s      LST: %s         ObjID:   --" % (sexstring(self.RaC/15.0/3600,fixed=True), sexstring(self.Time.LST, fixed=True), self.ObjID)
+      l2 = "Top Dec: %s     UT:  %s" %                      (sexstring(self.DecC/3600,fixed=True), self.Time.UT.time().isoformat()[:-4])
+      l3 = "Alt:     %s      HA:  %s        ObjRA:   --" %  (sexstring(self.Alt, fixed=True), sexstring(self.RaC/15/3600-self.Time.LST, fixed=True))
+      l4 = "Airmass: %6.4f        NonSidereal: %s      ObjDec:  --" % (1/math.cos((90-self.Alt)/180*math.pi), {False:"OFF", True:" ON"}[prefs.NonSidOn])
+    else:
+      l1 = "Top RA:  %s      LST: %s         ObjID:   %s" % (sexstring(self.RaC/15.0/3600,fixed=True), sexstring(self.Time.LST, fixed=True), self.ObjID)
+      l2 = "Top Dec: %s     UT:  %s" %                      (sexstring(self.DecC/3600,fixed=True), self.Time.UT.time().isoformat()[:-4])
+      l3 = "Alt:     %s      HA:  %s        ObjRA:   %s" % (sexstring(self.Alt, fixed=True), sexstring(self.RaC/15/3600-self.Time.LST, fixed=True),
+                                                            sexstring(self.Ra/15/3600, fixed=True))
+      l4 = "Airmass: %6.4f        NonSidereal: %s      ObjDec:  %s" % (1/math.cos((90-self.Alt)/180*math.pi), {False:"OFF", True:" ON"}[prefs.NonSidOn],
+                                                                       sexstring(self.Dec/3600, fixed=True)
     l5 = "Moving:  %s           Frozen: %s           ObjEpoch:%6.1f" % ({False:" No", True:"Yes"}[motion.motors.Moving], {False:" No", True:"Yes"}[motion.motors.Frozen],
                                                                   self.Epoch)
     l6 = "Dome:  %s        Dome Tracking: %s" % ({False:"Inactive", True:"  Active"}[pdome.dome.DomeInUse or pdome.dome.ShutterInUse],
