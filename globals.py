8618888e86ac961784a28f846814f293595f16b9
MJUO <mjuo@cynosure.(none)>
2014-01-20 18:15:10 +1300

    Change loglevel from DEBUG to INFO

--- globals.py
+++ globals.py
@@ -56,8 +56,8 @@ DUMMYPADDLES = []
 
 CPPATH = ['/usr/local/etc/teljoy.ini', './teljoy.ini', '/home/mjuo/teljoy/teljoy.ini']    # Initialisation file path
 
-LOGLEVEL_CONSOLE = logging.DEBUG     # Logging level for console messages (INFO, DEBUG, ERROR, CRITICAL, etc)
-LOGLEVEL_LOGFILE = logging.DEBUG      # Logging level for logfile
+LOGLEVEL_CONSOLE = logging.INFO     # Logging level for console messages (INFO, DEBUG, ERROR, CRITICAL, etc)
+LOGLEVEL_LOGFILE = logging.INFO      # Logging level for logfile
 LOGFILE = "/tmp/teljoy.log"
 
 # create global logger object for Facility Controller
