6c0655225b5996b319a5776aa1066ff2f9671786
Andrew Williams <andrew@longtable.org>
2014-01-22 13:42:05 +0800

    Added pydoc generated documentation

--- docs/mkdoc.sh
+++ docs/mkdoc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-export PYTHONPATH=..:../extras
-pydoc -w ../controller.py*
+export PYTHONPATH=..:../extras:../cgi/secure
+cp ../teljoy.ini .
+pydoc -w ../controller.py
 pydoc -w ../correct.py
 pydoc -w ../detevent.py
 pydoc -w ../digio.py
@@ -11,24 +12,15 @@ pydoc -w ../nzdome.py
 pydoc -w ../pdome.py
 pydoc -w ../pyephem.py
 pydoc -w ../sqlint.py
-pydoc -w ../teljoy.py*
+pydoc -w ../teljoy.py
 pydoc -w ../tjserver.py
 pydoc -w ../usbcon.py
 pydoc -w ../utils.py
 pydoc -w ../weather.py
-pydoc -w ../extras/reset.py*
+pydoc -w ../extras/reset.py
 pydoc -w ../extras/tjclient.py
-pydoc -w ../extras/tjguide.py*
-pydoc -w ../extras/tjjump.py*
-pydoc -w ../extras/tjoffset.py*
-pydoc -w ../extras/tjpos.py*
-pydoc -w ../extras/tjreset.py*
-pydoc -w ../cgi/objects.py
-pydoc -w ../cgi/bncstat*
-pydoc -w ../cgi/al/aobjlist*
-pydoc -w ../cgi/al/aobjlist.html
-pydoc -w ../cgi/secure/htmlutil.py
-pydoc -w ../cgi/secure/__init__.py
-pydoc -w ../cgi/secure/nobjedit*
-pydoc -w ../cgi/secure/nobjedithtml.py
-pydoc -w ../cgi/secure/nobjedit.py
+pydoc -w ../extras/tjguide.py
+pydoc -w ../extras/tjjump.py
+pydoc -w ../extras/tjoffset.py
+pydoc -w ../extras/tjpos.py
+pydoc -w ../extras/tjreset.py
